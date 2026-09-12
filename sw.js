9c913736f04bfdecbe2f02d908957096bb209ea4
Viacheslav Kyrychenko <kirichenko.vyacheslav16@google.com>
2026-09-12 15:05:01 +0200

    Darken app header

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const APP_VERSION = "2.0.16";
+const APP_VERSION = "2.0.18";
 const CACHE_NAME = `miy-cykl-${APP_VERSION}`;
 
 const PRECACHE = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=2.0.16",
-  "./app.js?v=2.0.16",
+  "./styles.css?v=2.0.18",
+  "./app.js?v=2.0.18",
   "./storage.js",
   "./diagnostic.js",
   "./probuzhdenie-zones-guide.pdf",
