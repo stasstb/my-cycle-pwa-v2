af9fd4c3f79d0d22f5eea9f22226b004a41e5eb7
Viacheslav Kyrychenko <kirichenko.vyacheslav16@google.com>
2026-09-12 14:43:29 +0200

    Expand in-app help section

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const APP_VERSION = "2.0.10";
+const APP_VERSION = "2.0.12";
 const CACHE_NAME = `miy-cykl-${APP_VERSION}`;
 
 const PRECACHE = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=2.0.10",
-  "./app.js?v=2.0.10",
+  "./styles.css?v=2.0.12",
+  "./app.js?v=2.0.12",
   "./storage.js",
   "./diagnostic.js",
   "./probuzhdenie-zones-guide.pdf",
